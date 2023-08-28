8367c7e1b07e266c7547831f6a0b373d84127403
Guilherme Brito <guieduardo80@gmail.com>
2023-08-27 23:59:01 -0300

    Incluir novos arquivos

--- projeto/urls.py
+++ projeto/urls.py
@@ -13,17 +13,17 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    //path('sobre/', my_view),
+    //from django.http import HttpResponse
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import include, path  
 
 
-def my_view(request):
-    
-    return HttpResponse('AMO ANDREA')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', include('recipes.urls')), 
+    
 ]
