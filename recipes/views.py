68639f93b0a5ada80f88cc1a88a314f7dc1a3ce5
Guilherme Brito <guieduardo80@gmail.com>
2023-09-03 16:40:32 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 # Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates 
 
@@ -8,9 +7,5 @@ def home(request):
         'name': 'Guilherme Brito', 
     })
 
-def sobre(request):
-    return HttpResponse('SOBRE')
 
-def contato(request):
-    return render(request,'recipes/contato.html')
 
