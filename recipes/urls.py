2e9ce03c8946fd77293b05ed2b44b81565711383
Guilherme Brito <guieduardo80@gmail.com>
2023-09-22 01:05:21 -0300

    Adicionando arquivo utils

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-# Oponto indica a pasta recipes.E a linha inteira esta importando o modulo todo, não  só um arquivo do modulo viwes
+# O ponto indica a pasta recipes.vE a linha inteira esta importando o modulo todo, não  só um arquivo do modulo viwes
 
 # O <parametro> é o parâmetro a ser seguido pela UR, determinada pela view 
 
