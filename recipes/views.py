a14f706c3c95c57437e267a6de8dda07b60962da
Guilherme Brito <guieduardo80@gmail.com>
2023-09-23 01:32:00 -0300

    Adicionando conteudo dinamico nos cards

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-# Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates 
+# Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
+from utils.recipes.factory import make_recipe 
 
 
 def home(request):
     return render(request,'recipes/pages/home.html',context={
-        'name': 'Guilherme Brito', 
+        'recipes': [make_recipe() for _ in range(10)], 
 })
 
 def recipe(request,id):
     return render(request,'recipes/pages/recipe-view.html',context={
-        'name': 'Guilherme Brito', 
+        'recipe': make_recipe(), 
 })
 
 
