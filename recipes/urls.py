f39c860c02af772161c915df34e0e0c3eef05e4b
Guilherme Brito <guieduardo80@gmail.com>
2023-09-27 23:43:49 -0300

    Tirando o botão de mais sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-# O ponto indica a pasta recipes.vE a linha inteira esta importando o modulo todo, não  só um arquivo do modulo viwes
+# O ponto indica a pasta recipes.vE a linha inteira esta importando o modulo todo, não  só um arquivo do modulo views
 
 # O <parametro> é o parâmetro a ser seguido pela UR, determinada pela view 
 
