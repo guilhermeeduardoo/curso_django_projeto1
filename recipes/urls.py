f97d7cd5039a0c242d574e46f10bf11a8934e9dd
Guilherme Brito <guieduardo80@gmail.com>
2023-10-01 01:23:24 -0300

    Ajeitando as urls do site

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 # O <parametro> é o parâmetro a ser seguido pela UR, determinada pela view 
 
+# O name="", é usado para dar nomes unicos as URLS
+
+
+app_name = 'recipes' # app_name:name, é a variavel com o nome da url
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),    
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),    
 ]
 
