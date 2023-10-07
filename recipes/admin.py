b29072ebfdcb25b588b09a9d510241c356c56597
Guilherme Brito <guieduardo80@gmail.com>
2023-10-07 00:46:23 -0300

    Registrando o modelo de recipe

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 # Faz uma classe, para registrar o modelo Category e coloca essa linha admin.site.register(Category, CategoryAdmin)
 
+# @admin.register(Recipe) é outra forma de registrar um model no banco de dados, usando o decorate
+
 class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RcipeAdmin(admin.ModelAdmin):
     ...
 
 
