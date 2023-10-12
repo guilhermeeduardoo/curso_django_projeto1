2c99d883b9ff35ab6e14dd4722f16419d1dab231
Guilherme Brito <guieduardo80@gmail.com>
2023-10-12 01:53:36 -0300

    Criando o category_id

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+from .models import Recipe
+
 # Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
-from utils.recipes.factory import make_recipe 
+# recipes = Recipe.objects.all(), é usado para pegar todas as receitas criadas no models, que nem no python shell
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request,'recipes/pages/home.html',context={
-        'recipes': [make_recipe() for _ in range(10)], 
+        'recipes': recipes, 
+})
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request,'recipes/pages/home.html',context={
+        'recipes': recipes, 
 })
 
 def recipe(request,id):
