51d074113e859024dc23f61ad68ce96fc796b6f2
Guilherme Brito <guieduardo80@gmail.com>
2023-10-13 00:51:47 -0300

    Colocando o nome da pagina de category

--- recipes/views.py
+++ recipes/views.py
@@ -4,17 +4,18 @@ from .models import Recipe
 
 # Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
 # recipes = Recipe.objects.all(), é usado para pegar todas as receitas criadas no models, que nem no python shell
+# filter(is_published=True), faz com que só apareça as receitas que estejam com o is_published marcado
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request,'recipes/pages/home.html',context={
         'recipes': recipes, 
 })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request,'recipes/pages/home.html',context={
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request,'recipes/pages/category.html',context={
         'recipes': recipes, 
 })
 
