eae8a86eb7b90e75b85c4094afae9ff759441179
Guilherme Brito <guieduardo80@gmail.com>
2023-10-17 01:02:53 -0300

    Mudando o nome das paginas

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
@@ -5,6 +6,9 @@ from .models import Recipe
 # Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
 # recipes = Recipe.objects.all(), é usado para pegar todas as receitas criadas no models, que nem no python shell
 # filter(is_published=True), faz com que só apareça as receitas que estejam com o is_published marcado
+# O f, é o f string, que passa o argumento como string e o recipes.first().category.name, pega o primeiro nome a ser chamado quando clicado na categoria
+# O - Category |, é só pra complemantar o nome da página, junta uma variavel com uma string
+# Esse código, if not recipes: raise Http404('Not found :('), foi feito para retornar um 404 e a mensagem "Not found", quando não achar um id de categoria
 
 
 def home(request):
@@ -15,8 +19,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found :(') 
+
     return render(request,'recipes/pages/category.html',context={
-        'recipes': recipes, 
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |' 
 })
 
 def recipe(request,id):
