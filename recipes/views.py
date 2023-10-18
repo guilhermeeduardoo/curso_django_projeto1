b6d4e163e0c407ca2989b36120196b8b40a9b070
Guilherme Brito <guieduardo80@gmail.com>
2023-10-18 00:21:06 -0300

    Colocando o getlisor404

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
-# Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
-# recipes = Recipe.objects.all(), é usado para pegar todas as receitas criadas no models, que nem no python shell
-# filter(is_published=True), faz com que só apareça as receitas que estejam com o is_published marcado
-# O f, é o f string, que passa o argumento como string e o recipes.first().category.name, pega o primeiro nome a ser chamado quando clicado na categoria
-# O - Category |, é só pra complemantar o nome da página, junta uma variavel com uma string
-# Esse código, if not recipes: raise Http404('Not found :('), foi feito para retornar um 404 e a mensagem "Not found", quando não achar um id de categoria
 
 
 def home(request):
@@ -18,14 +11,12 @@ def home(request):
 })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not found :(') 
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
 
     return render(request,'recipes/pages/category.html',context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category |' 
+        'title': f'{recipes[0].category.name} - Category |' 
 })
 
 def recipe(request,id):
@@ -36,3 +27,15 @@ def recipe(request,id):
 
 
 
+
+
+
+
+
+# Render faz a função de dar um request e fazer o HttpResponse, colocando o nome do arquivo html a ser renderizado em templates
+# recipes = Recipe.objects.all(), é usado para pegar todas as receitas criadas no models, que nem no python shell
+# filter(is_published=True), faz com que só apareça as receitas que estejam com o is_published marcado
+# O f, é o f string, que passa o argumento como string e o recipes.first().category.name, pega o primeiro nome a ser chamado quando clicado na categoria
+# O - Category |, é só pra complemantar o nome da página, junta uma variavel com uma string
+# Esse código, if not recipes: raise Http404('Not found :('), foi feito para retornar um 404 e a mensagem "Not found", quando não achar um id de categoria
+# Com o get_list_or_404 em vez do first que retorna uma queryset, usa o [0], já que vai percorrer uma lista, a vantagem é que não precisa usar o if. Passa o model nesse caso o Recipe e os filtros
