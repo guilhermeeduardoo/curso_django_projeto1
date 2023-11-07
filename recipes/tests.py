3ab7f7634e93c428baddc5600d246a71f5af1ca1
Guilherme Brito <guieduardo80@gmail.com>
2023-11-07 01:55:57 -0300

    colocando os testes para funcionar

--- recipes/tests.py
+++ recipes/tests.py
@@ -2,4 +2,6 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        variavel = '12345'
+        print('OLÁ MUNDO')
+        assert 1 == 1, 'um é igual a um'
